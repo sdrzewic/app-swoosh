6824607936733911780ddfedc2487affe8b244f9
Steve Drzewiczewski <steve@stevedandrebecca.com>
2018-02-26 00:40:12 -0500

    end of lesson 8

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -16,11 +16,11 @@ class LeagueVC: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
+
 
     /*
     // MARK: - Navigation
