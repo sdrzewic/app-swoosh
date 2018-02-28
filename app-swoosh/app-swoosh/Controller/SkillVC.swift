f81032f868d9c8d3ac16057fc65fdc77b5bd3f2d
Steve Drzewiczewski <steve@stevedandrebecca.com>
2018-02-27 19:52:28 -0500

    Lesson 10 in the books

--- app-swoosh/app-swoosh/Controller/SkillVC.swift
+++ app-swoosh/app-swoosh/Controller/SkillVC.swift
@@ -10,10 +10,11 @@ import UIKit
 
 class SkillVC: UIViewController {
 
+    var player: Player!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        print(player.desiredLeague)
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,14 +23,5 @@ class SkillVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
